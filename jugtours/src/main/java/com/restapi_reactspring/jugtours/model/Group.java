c882a13b9f020b7696f15869010b9fc9e8e74931
Edson Rodrigues <edson091990@gmail.com>
2024-06-09 18:32:04 -0300

    Springboot initial project and Group.java file

---
+++ jugtours/src/main/java/com/restapi_reactspring/jugtours/model/Group.java
@@ -0,0 +1,41 @@
+package com.restapi_reactspring.jugtours.model;
+
+import java.util.Set;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.w3c.dom.events.Event;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name="user_group")
+public class Group {
+  @Id
+  @GeneratedValue
+  private Long id;
+  @NonNull
+  private String name;
+  private String address;
+  private String city;
+  private String stateOrProvince;
+  private String country;
+  private String postalCode;
+  @ManyToOne(cascade = CascadeType.PERSIST)
+  private User user;
+  @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  private Set<Event> events;
+}
